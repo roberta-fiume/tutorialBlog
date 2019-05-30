5c5c3a716696c79614b7db7bb3e38d64aa200e05
Roberta <robertaf898@gmail.com>
2019-05-30 12:31:51 +0100

    adds old tutorial for blog

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <div id="app">
+       <h1> I'm the parent </h1>
+       <h3 id="name"> My name is: {{ name}}</h3>
+       <button id="button1" v-on:click="changeName"> Change name in my son</button>
+    <child-component :nameAsProp="name"></child-component>
+  </div>
+</template>
+
+<script>
+ import ChildComponent from  "./ChildComponent.vue";
+  export default {
+  data() {
+    return {
+      name: 'Jack'
+    }
+  },
+
+  methods: {
+    changeName() {
+     return this.nameAsProp;
+      console.log("name",this.nameAsProp)
+    }
+  },
+
+  components: {
+    "child-component": ChildComponent
+  }
+}
+</script>
+
+<style>
+
+@import url('https://fonts.googleapis.com/css?family=Raleway:100,300,500,700');
+
+html, body {
+  font-family: 'Raleway', sans-serif;
+}
+
+body {
+  font-family: 'Raleway', sans-serif;
+}
+
+#app {
+  width: 900px;
+  height: 700px;
+  background-color: lightseagreen;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-family: 'Itim', cursive;
+  border: 2px solid rgb(20, 98, 80);
+}
+
+#name {
+  font-size: 18px; 
+}
+
+
+#button1 {
+  width: 200px;
+  height: 40px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-family: 'Itim', cursive;
+  background-color: #6AC0BD;
+  border: 2px solid #0F4442;
+  margin-bottom: 30px;
+} 
+</style>
---
+++ src/ChildComponent.vue
@@ -0,0 +1,48 @@
+<template>
+    <div id="son">
+       <h1> I'm the child </h1>
+       <h3 id="h3"> My name is: {{ nameAsProp }}</h3>
+    </div>
+</template>
+
+<script>
+export default {
+
+    props: ['nameAsProp'],
+
+    data() {
+        return {
+            name: 'Ariel'
+        }
+  },
+
+  methods: {
+      nameFromParent() {
+          return this.nameAsProp;
+      }
+  }
+}
+</script>
+
+
+<style>
+ #son {
+     width: 300px;
+     height: 300px;
+     background-color: lightcoral;
+     font-size: 12px;
+     display: flex;
+     flex-direction: column;
+     justify-content: center;
+     align-items: center;
+     border: 2px solid rgb(208, 32, 61)
+    }
+
+    #h3 {
+        font-size: 15px; 
+    }
+
+    @import url('https://fonts.googleapis.com/css?family=Itim');
+</style>
+
+
